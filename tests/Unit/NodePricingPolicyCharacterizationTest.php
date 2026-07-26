7e1bed4d2223ace1847e578b4a621ca6061172d3
RobLe3 <github@roblemumin.com>
2026-07-26 21:40:42 +0200

    refactor: extract node pricing policy

--- tests/Unit/NodePricingPolicyCharacterizationTest.php
+++ tests/Unit/NodePricingPolicyCharacterizationTest.php
@@ -5,6 +5,7 @@
 namespace Tests\Unit;
 
 use App\Services\JwtService;
+use App\Services\NodePricingPolicy;
 use App\Services\NodeRegistry;
 use App\Services\OperatorDelegationVerifier;
 use Illuminate\Validation\ValidationException;
@@ -119,6 +120,7 @@ class NodePricingPolicyCharacterizationTest extends TestCase
         return new NodeRegistry(
             $this->createMock(JwtService::class),
             $this->createMock(OperatorDelegationVerifier::class),
+            new NodePricingPolicy,
         );
     }
 }
