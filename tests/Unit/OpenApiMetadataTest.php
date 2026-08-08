0d7fac1e7c072d5132fbee5ee22d704ff71aabe4
RobLe3 <github@roblemumin.com>
2026-08-08 11:47:13 +0200

    release: prepare PHP directory v1.10.89

--- tests/Unit/OpenApiMetadataTest.php
+++ tests/Unit/OpenApiMetadataTest.php
@@ -23,7 +23,7 @@ class OpenApiMetadataTest extends TestCase
             $openApi,
         );
 
-        foreach (['openapi: 3.1.0', 'info.version: 1.7.0', 'v1.10.88', 'v1.10.76.2'] as $version) {
+        foreach (['openapi: 3.1.0', 'info.version: 1.7.0', 'v1.10.89', 'v1.10.76.2'] as $version) {
             $this->assertStringContainsString($version, $readme);
         }
     }
